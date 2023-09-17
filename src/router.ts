e11c09817166c67a5e09a609cfbbfd261a23989e
Hau Pham <hauph@hotmail.com>
2023-09-17 19:40:51 +0700

    feat: remove github action + update router

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import AdminPage from './pages/Admin/Admin.vue';
 import HomePage from './pages/Home/Home.vue';
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
